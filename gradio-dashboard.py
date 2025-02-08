7e69175ddd72c7df12f92f07ab010af09076963a
RistoFlink <kenssi@gmail.com>
2025-02-08 15:51:39 +0200

    Update launch code to try and get deployment to work

--- gradio-dashboard.py
+++ gradio-dashboard.py
@@ -32,7 +32,6 @@ def retrieve_semantic_recommendations(
         initial_top_k: int = 50,
         final_top_k: int = 16,
 ) -> pd.DataFrame:
-
     recs = db_books.similarity_search(query, k=initial_top_k)
     books_list = [int(rec.page_content.strip('"').split()[0]) for rec in recs]
     book_recs = books[books["isbn13"].isin(books_list)].head(initial_top_k)
@@ -81,26 +80,28 @@ def recommend_books(
         results.append((row["large_thumbnail"], caption))
     return results
 
+
 categories = ["All"] + sorted(books["simple_categories"].unique())
 tones = ["All"] + ["Happy", "Surprising", "Angry", "Suspenseful", "Sad"]
 
-with gr.Blocks(theme = gr.themes.Ocean()) as dashboard:
+with gr.Blocks(theme=gr.themes.Ocean()) as dashboard:
     gr.Markdown("# Semantic book recommender")
 
     with gr.Row():
-        user_query = gr.Textbox(label = "Please enter a description of a book:",
-                                placeholder = "e.g., A story about pirates")
-        category_dropdown = gr.Dropdown(choices = categories, label = "Select a category:", value = "All")
-        tone_dropdown = gr.Dropdown(choices = tones, label = "Select an emotional tone:", value = "All")
+        user_query = gr.Textbox(label="Please enter a description of a book:",
+                                placeholder="e.g., A story about pirates")
+        category_dropdown = gr.Dropdown(choices=categories, label="Select a category:", value="All")
+        tone_dropdown = gr.Dropdown(choices=tones, label="Select an emotional tone:", value="All")
         submit_button = gr.Button("Find recommendations")
 
     gr.Markdown("## Recommendations")
-    output = gr.Gallery(label = "Recommended books", columns = 8, rows = 2)
+    output = gr.Gallery(label="Recommended books", columns=8, rows=2)
 
-    submit_button.click(fn = recommend_books,
-                        inputs = [user_query, category_dropdown, tone_dropdown],
-                        outputs = output)
-
+    submit_button.click(fn=recommend_books,
+                        inputs=[user_query, category_dropdown, tone_dropdown],
+                        outputs=output)
 
 if __name__ == "__main__":
-    dashboard.launch()
+    import os
+
+    dashboard.launch(server_port=int(os.environ.get("PORT", 7860)), server_name="0.0.0.0")
